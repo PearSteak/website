6743f568347c6d33b08c38041826f6755abf2514
PearSteak <>
2020-12-02 19:40:10 +0100

    .

--- js/main.js
+++ js/main.js
@@ -15,3 +15,39 @@ window.addEventListener('load', () => {
 if (window.ethereum !== undefined) {
 	window.ethereum.enable();
 };
+
+$(function(){
+	$(".pear-flexible").click(function(){
+		$(".pear-dropdown").html("Flexible <span class=\"caret\"></span>");
+		$(".pear-dropdown").val(0);
+	});
+	$(".pear-7days").click(function(){
+		$(".pear-dropdown").html("7-days <span class=\"caret\"></span>");
+		$(".pear-dropdown").val(1);
+	});
+	$(".pear-30days").click(function(){
+		$(".pear-dropdown").html("30-days <span class=\"caret\"></span>");
+		$(".pear-dropdown").val(2);
+	});
+	$(".pear-365days").click(function(){
+		$(".pear-dropdown").html("365-days <span class=\"caret\"></span>");
+		$(".pear-dropdown").val(3);
+	});
+	
+	$(".steak-flexible").click(function(){
+		$(".steak-dropdown").html("Flexible <span class=\"caret\"></span>");
+		$(".steak-dropdown").val(0);
+	});
+	$(".steak-7days").click(function(){
+		$(".steak-dropdown").html("7-days <span class=\"caret\"></span>");
+		$(".steak-dropdown").val(1);
+	});
+	$(".steak-30days").click(function(){
+		$(".steak-dropdown").html("30-days <span class=\"caret\"></span>");
+		$(".steak-dropdown").val(2);
+	});
+	$(".steak-365days").click(function(){
+		$(".steak-dropdown").html("365-days <span class=\"caret\"></span>");
+		$(".steak-dropdown").val(3);
+	});
+});
