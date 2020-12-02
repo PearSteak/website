066ed6616de6a19dc937123a528e9fdf598e551a
PearSteak <>
2020-12-02 20:04:10 +0100

    .

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function updateTotalSupply() {
 			
 	pear_contract.getTotalSupply.call(function(error, info) {
 		if (!error) {
-			console.log(info["c"][0]);
+			$(".pearTotalSupply_num").text(info["c"][0]);
 		} else {
 			console.log(error);
 		}
@@ -38,7 +38,7 @@ function updateTotalSupply() {
 	
 	steak_contract.getTotalSupply.call(function(error, info) {
 		if (!error) {
-			console.log(info["c"][0]);
+			$(".steakTotalSupply_num").text(info["c"][0]);
 		} else {
 			console.log(error);
 		}
