1a0458ad9635bb8370a1d0f4019ebc5508343e88
PearSteak <>
2020-12-03 17:18:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -166,7 +166,7 @@ function updatePearStakeList() {
 	pear_contract.getStakes.call(function(error, info) {
 		if (!error) {
 			$.each(info, function( index, value ) {
-				console.log(value[0]);
+				console.log(value[0]["c"]);
 			});
 		} else {
 			console.log(error);
