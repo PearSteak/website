8b98121582081911eada7e3d3efbfc7a8d680ce6
PearSteak <>
2020-12-16 13:43:29 +0100

    Update main.js

--- lp/js/main.js
+++ lp/js/main.js
@@ -186,7 +186,7 @@ function updatePearLPBalance() {
 	pear_uniswap_contract.balanceOf(account, function(error, info) {
 		if (!error) {
 			console.log(info);
-			$(".pearLP-amount").attr("placeholder", (info/1000000000000000000).toFixed(2));
+			$(".pearLP-amount").attr("placeholder", (info).toFixed(2));
 		} else {
 			console.log(error);
 		}
