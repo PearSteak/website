4ba22ff3d2e19e4c462cadf174926b8b527ad4d6
PearSteak <>
2020-12-16 13:42:34 +0100

    Update main.js

--- lp/js/main.js
+++ lp/js/main.js
@@ -12,8 +12,10 @@ window.addEventListener('load', () => {
 		: '0x0000000000000000000000000000000000000001';
 		
 	pear_contract = web3.eth.contract(pear_abi).at(pear_contract_address);
+	pear_uniswap_contract = web3.eth.contract(pear_abi).at(pear_uniswap_contract_address);
 	pearLP_contract = web3.eth.contract(pearLP_abi).at(pearLP_contract_address);
 	steak_contract = web3.eth.contract(steak_abi).at(steak_contract_address);
+	steak_uniswap_contract = web3.eth.contract(steak_abi).at(steak_uniswap_contract_address);
 	steakLP_contract = web3.eth.contract(steakLP_abi).at(steakLP_contract_address);
 	sales_contract = web3.eth.contract(sales_abi).at(sales_contract_address);
 	
@@ -169,6 +171,22 @@ function updatePearBalance() {
 	pear_contract.getMyBalance.call(function(error, info) {
 		if (!error) {
 			$(".pear-amount").attr("placeholder", (info/1000000000000000000).toFixed(2));
+		} else {
+			console.log(error);
+		}
+	});
+};
+
+function updatePearLPBalance() {
+	var account =
+		web3.eth.accounts !== undefined && web3.eth.accounts[0] !== undefined
+			? web3.eth.accounts[0]
+			: '0x0000000000000000000000000000000000000001';
+			
+	pear_uniswap_contract.balanceOf(account, function(error, info) {
+		if (!error) {
+			console.log(info);
+			$(".pearLP-amount").attr("placeholder", (info/1000000000000000000).toFixed(2));
 		} else {
 			console.log(error);
 		}
